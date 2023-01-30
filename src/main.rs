d2ef0a5661d484a0c4146dc601bbd51138e71168
Rick Hull <rick.hull@gmail.com>
2023-01-30 18:39:47 -0500

    just build and run hello world

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-#![feature(proc_macro_hygiene, decl_macro)] // language features needed by Rocket
-
-// Import the rocket macros
-#[macro_use]
-extern crate rocket;
-
-// Create route / that returns "Hello, world!"
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
-    println!("Goodbye, world!");
+    println!("Hello, world!");
 }
