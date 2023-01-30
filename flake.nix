a85a7ed8f0c8ce1f408c0bde220aaf1d119227d6
Rick Hull <rick.hull@gmail.com>
2023-01-30 15:29:21 -0500

    WIP

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Provides a shell with rust (language) available";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell  = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = [
+          pkgs.rust
+        ];
+      };
+    });
+}
